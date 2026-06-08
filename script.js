c375675bb73c25aa454317f57044314f21ae2fbf
mallelasravani26-maker <mallelasravani26@gmail.com>
2026-06-08 11:05:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function validateForm(){
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let message = document.getElementById("message").value;
+
+if(name === "" || email === "" || message === ""){
+    alert("Please fill all fields");
+    return false;
+}
+
+alert("Form Submitted Successfully");
+return false;
+}
